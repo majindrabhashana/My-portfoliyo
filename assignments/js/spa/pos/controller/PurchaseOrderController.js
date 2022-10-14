1cc4b698629f9277753dad090e7845931bd169d9
majindrabhashana <majindrabhashana@gmail.com>
2022-10-14 10:34:32 -0700

    order js update

---
+++ assignments/js/spa/pos/controller/PurchaseOrderController.js
@@ -0,0 +1,13 @@
+function loadAllCustomersForOption() {
+    $("#selectCustomerID").empty();
+    for (let cus of customers) {
+        $("#selectCustomerID").append(`<option>${cus.id}</option>`);
+    }
+
+}
+
+$("#selectCustomerID").click(function (){
+    let id = $("#selectCustomerID").val();
+    let search = searchCustomer(id);
+    $("#orderCustomerName").val(search.name);
+});
