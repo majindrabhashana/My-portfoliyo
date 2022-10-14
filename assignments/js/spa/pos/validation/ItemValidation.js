7a5a89a8d6920f374c784acc9d115ca7bcd52fe8
majindrabhashana <majindrabhashana@gmail.com>
2022-10-14 04:15:16 -0700

    item regex update

--- assignments/js/spa/pos/validation/ItemValidation.js
+++ assignments/js/spa/pos/validation/ItemValidation.js
@@ -1,1 +1,6 @@
 $("#txtItemCode").focus();
+
+const itemCodeRegEx = /^(I00-)[0-9]{1,3}$/;
+const itemNameRegEx = /^[A-z ]{5,20}$/;
+const itemQtyRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+const itemUnitPriceRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
