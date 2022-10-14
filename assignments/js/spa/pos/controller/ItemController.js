764f81b219d40d994428d936ebf118a53324b087
majindrabhashana <majindrabhashana@gmail.com>
2022-10-14 12:27:50 -0700

    item js add

--- assignments/js/spa/pos/controller/ItemController.js
+++ assignments/js/spa/pos/controller/ItemController.js
@@ -5,13 +5,8 @@ $("#saveItem").click(function (){
     let qty = $("#txtItemQty").val();
     let unitPrice = $("#txtItemUnitPrice").val();
 
-    var itemObject = {
-        code: itemCode,
-        itemName: itemName,
-        qty: qty,
-        unitPrice: unitPrice,
-
-    }
+    var item = saveItem(itemCode,itemName,qty,unitPrice);
+    items.push(item);
 
     items.push(itemObject);
 
---
+++ assignments/js/spa/pos/model/Item.js
@@ -0,0 +1,8 @@
+function saveItem(itemCode,itemDescription,itemQty,unitPrice){
+    return{
+        code:itemCode,
+        itemName:itemDescription,
+        qty:itemQty,
+        unitPrice:unitPrice
+    };
+}
