d161dc2ab52961c53d0f79051064c3c42149d905
majindrabhashana <majindrabhashana@gmail.com>
2022-10-13 21:17:44 -0700

    customer buttons events added

---
+++ assignments/js/spa/pos/controller/CustomerController.js
@@ -0,0 +1,8 @@
+$("#saveCustomer").click(function () {
+    let customerID = $("#txtCustomerID").val();
+    let customerName = $("#txtCustomerName").val();
+    let customerAddress = $("#txtCustomerAddress").val();
+    let customerContact = $("#txtCustomerContact").val();
+    let customerSalary = $("#txtCustomerSalary").val();
+
+})
