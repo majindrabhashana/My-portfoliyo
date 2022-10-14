f64532c9c078c0affa359cb2d1428933f203a346
majindrabhashana <majindrabhashana@gmail.com>
2022-10-14 00:31:16 -0700

    search customer update

--- assignments/js/spa/pos/controller/CustomerController.js
+++ assignments/js/spa/pos/controller/CustomerController.js
@@ -23,17 +23,40 @@ function loadAllCustomers() {
     $("#tblCustomer").empty();
 
     for (var customer of customers){
-        var cusData = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td></tr>`;
+        var all= `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td><td><button class="btn btn-warning btn-mini" data-bs-target="#editCustomers"
+                        data-bs-toggle="modal" id="btn-edit"><i class="fa-solid fa-pen-to-square"></i> Edit
+                        </button>
+                        <button class="btn btn-danger btn-mini delete"><i class="fa-solid fa-trash"></i> Delete</button>
+                        </td>
+                    </tr>`;
+        $("#tblCustomer").append(all);
 
     }
 
-
 }
-
 function searchCustomer(cusID) {
     for (let customer of customers) {
         if (customer.id == cusID) {
             return customer;
         }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function clearData() {
+    $("#txtCustomerID").val("");
+    $("#txtCustomerName").val("");
+    $("#txtCustomerAddress").val("");
+    $("#txtCustomerContact").val("");
+    $("#txtCustomerSalary").val("");
 }
