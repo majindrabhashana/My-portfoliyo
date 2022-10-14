e563fc86fec92153539dd55306bb52df771196f0
majindrabhashana <majindrabhashana@gmail.com>
2022-10-14 00:15:38 -0700

    customercontroller update

--- assignments/js/spa/pos/controller/CustomerController.js
+++ assignments/js/spa/pos/controller/CustomerController.js
@@ -24,7 +24,16 @@ function loadAllCustomers() {
 
     for (var customer of customers){
         var cusData = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td></tr>`;
+
     }
 
 
+}
+
+function searchCustomer(cusID) {
+    for (let customer of customers) {
+        if (customer.id == cusID) {
+            return customer;
+        }
+    }
 }
